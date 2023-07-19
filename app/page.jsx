f687498adc40dac1ee77dcb3b2ddb21969693b5f
rpiflv <rpiflv@gmail.com>
2023-07-19 11:35:45 +0900

    add Home description

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,15 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+        </h1>
+        <p className="desc text-center">
+            mAI-chatGPT is an open source tool to share and discover creative prompts
+        </p>
+    </section>
   )
 }
 
