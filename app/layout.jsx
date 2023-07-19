da128ec7bc01aadaeb302cb8211a524fea55d790
rpiflv <rpiflv@gmail.com>
2023-07-19 10:31:17 +0900

    render Home

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/global.css';
+
+export const metadata = {
+    title: "mAIChatGPT",
+    description: "Dciscover and Share AI Prompts"
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient'/>
+            </div>
+            <main className='app'>
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
