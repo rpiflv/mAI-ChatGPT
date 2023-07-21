0b99ce309cfb82146112dd57c9698ce997300e49
rpiflv <rpiflv@gmail.com>
2023-07-21 11:49:49 +0900

    add copy prompt feat

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,7 +7,13 @@ import { usePathname, useRouter } from "next/navigation";
 
 function PromptCard({post, handleTagClick, handleEdit, handleDelete}) {
 
-  const [copied, setCopied] = useState('')
+  const [copied, setCopied] = useState('');
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(""), 2000)
+  }
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
@@ -28,7 +34,7 @@ function PromptCard({post, handleTagClick, handleEdit, handleDelete}) {
               </p>
           </div>
         </div>
-      <div className="copy_btn" onClick={() => {}}>
+      <div className="copy_btn" onClick={handleCopy}>
         <Image src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'}
           width={12}
           height={12}
