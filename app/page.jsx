9cdf9efe7b3272c8afda6e2e5d2b1860215973b4
rpiflv <rpiflv@gmail.com>
2023-07-22 16:38:05 +0900

    fix feed component in  home

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed"
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -9,6 +11,7 @@ const Home = () => {
         <p className="desc text-center">
             mAI-chatGPT is an open source tool to share and discover creative prompts
         </p>
+        <Feed/>
     </section>
   )
 }
