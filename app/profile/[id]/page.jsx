f59e4e7e70a55fb3f2022a732cbe83bcd620ebd7
rpiflv <rpiflv@gmail.com>
2023-07-23 13:57:41 +0900

    clean profile file

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,15 +1,11 @@
 "use client"
 
 import { useState, useEffect } from "react";
-// no need this file
-// import { useSession } from "next-auth/react";
 import Profile from "@components/Profile";
-
 import { useRouter } from "next/navigation";
+
 const UserProfile = ({params}) => {
   
-  // const {data: session} = useSession();
-
   const [posts, setPosts] = useState([]);
   const [owner, setOwner] = useState('');
 
@@ -25,32 +21,11 @@ const UserProfile = ({params}) => {
       if (params.id) fecthPosts();
     }, []);
 
-    // const handleEdit = (post) => {
-    //   router.push(`/update-prompt?id=${post._id}`)
-    // }
-
-    // const handleDelete = async (post) => {
-    //   const hasConfirmed = confirm("Are you sure to delete?");
-    //   if (hasConfirmed) 
-    //   try {
-    //     await fetch(`/api/prompt/${post._id}`, {
-    //       method: "DELETE"
-    //     });
-    //     const filteredPosts = posts.filter(p => p._id !== post._id);
-    //     setPosts(filteredPosts);
-    //   } catch (error) {
-    //     console.log(error)
-    //   }
-    // }
-
-
   return (
     <Profile 
         name={owner}
-        desc="Welcome to your profile"
+        desc={`Welcome to ${owner}'s profile`}
         data={posts}
-        // handleEdit={handleEdit}
-        // handleDelete={handleDelete}
     />
   )
 }
