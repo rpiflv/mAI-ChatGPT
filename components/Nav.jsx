f9ed3eea29ef705e381bcd2a53fca877f70d66db
rpiflv <rpiflv@gmail.com>
2023-07-24 11:05:03 +0900

    add transp logo

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -25,7 +25,7 @@ function Nav() {
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href={"/"} className='flex gap-2 flex-center'>
         <Image 
-        src="/assets/images/logo.png" 
+        src="/assets/images/logoTransp.png" 
         alt='logo'
         width={43}
         height={43}
