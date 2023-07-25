82c22e2e0c8726441ab0fe3b83ad6fa86999e4bb
rpiflv <rpiflv@gmail.com>
2023-07-25 22:56:44 +0900

    add favicon to layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,18 @@
 import '@styles/global.css';
 import Nav from '@components/Nav';
 import Provider from '@components/Provider';
+import Head from 'next/head';
+
 
 export const metadata = {
     title: "mAIChatGPT",
-    description: "Dciscover and Share AI Prompts"
+    description: "Dciscover and Share AI Prompts",
+    icons: {
+        icon: '/assets/icons/favicon.ico',
+      },
 }
+
+
 
 const RootLayout = ({children}) => {
   return (
@@ -15,6 +22,9 @@ const RootLayout = ({children}) => {
                 <div className='main'>
                     <div className='gradient'/>
                 </div>
+                    {/* <Head>
+                        <link rel="icon" href="../assets/icons/logo16x16.ico" />
+                    </Head> */}
                 <main className='app'>
                     <Nav />
                     {children}
