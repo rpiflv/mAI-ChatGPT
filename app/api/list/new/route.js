6e804f9e3d0b386f986ca69f72143cb74849afe9
rpiflv <rpiflv@gmail.com>
2023-07-29 18:10:32 +0900

    add route for new list

---
+++ app/api/list/new/route.js
@@ -0,0 +1,17 @@
+import connectToDB from "@utils/database";
+import MyList from "@models/myList";
+
+export const POST = async (req, res) => {
+    const {userId, listName} = await req.json();
+    try {
+        await connectToDB();
+        const newList = new MyList({
+            owner: userId, name: listName
+        })
+
+        await newList.save();
+        return new Response(JSON.stringify(newList), {status: 201})
+    } catch(error) {
+        return new Response("Failed to create a new list", {status: 500})
+    }
+}
