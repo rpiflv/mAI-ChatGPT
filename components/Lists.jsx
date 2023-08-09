bbf8600f5b709a121fce80859139bd3cf14a11d3
rpiflv <rpiflv@gmail.com>
2023-08-09 12:22:55 +0900

    add style to list page

--- components/Lists.jsx
+++ components/Lists.jsx
@@ -1,4 +1,4 @@
-"use client"
+import PromptCard from "./PromptCard"
 
 function Lists({ name, desc, posts, lists}) {
 
@@ -6,25 +6,27 @@ function Lists({ name, desc, posts, lists}) {
     <section className="w-full">
       <span className="blue_gradient"><h1 className="head_text text-left">{name} Lists</h1></span>
       <p className="desc text-left">{desc}</p>
-      <div className="mt-10 prompt-layout">
-      {lists?.map(list => (
-        <div key={list._id}>{list.name}
-        <div>
-          {posts.map(post => ( 
-            <div>
-              {post.list === list._id && 
-    <span>
-                {post.list}
-                {post.tag}
-    </span>
-              
-
-              }
+      <div className="flex mt-10 flex-col">
+        {lists?.map(list => (
+          <div key={list._id} className="relative w-full flex-row">
+            <span className="list_text mb-10">{list.name}</span> 
+            <div className="columns-1 md:columns-2
+             lg:columns-3 mb-10 mt-5 flex-row">
+              {posts.map(post => ( 
+                <div key={post._id} className="mr-5">
+                  {post.list === list._id && 
+                    <PromptCard 
+                    key={post._id}
+                    post={post}
+                    />
+                  }
+                </div>
+              ))}
+              <br/>
             </div>
-        ))}</div>
-        </div>
-      ))}
-    </div>
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
