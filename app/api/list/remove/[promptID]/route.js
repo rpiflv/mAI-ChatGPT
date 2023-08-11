96dffc7d8586bf446dc954fbe0429dba5ffd18a4
rpiflv <rpiflv@gmail.com>
2023-08-12 08:39:20 +0900

    change field type for list in prompt model

--- app/api/list/remove/[promptID]/route.js
+++ app/api/list/remove/[promptID]/route.js
@@ -6,7 +6,7 @@ export const PATCH = async (req, { params }) => {
         await connectToDB();
         const existingPrompt = await Prompt.findById(params.promptID);
         if (!existingPrompt) return new Response("Prompt not found", {status: 404})
-        existingPrompt.list = "";
+        existingPrompt.list = null;
         await existingPrompt.save();
         return new Response(JSON.stringify(existingPrompt), {
             status: 200
