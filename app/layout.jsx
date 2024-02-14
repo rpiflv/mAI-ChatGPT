37e41d9f43c4a8a8864e4c9abe1c2da9bdc88bb4
rpiflv <rpiflv@gmail.com>
2024-02-14 09:39:52 +0900

    add image

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,7 +17,7 @@ const RootLayout = ({children}) => {
         <head>
             <meta charSet='utf-8' />
             <meta name='viewport' content='width=device-width' />
-            <meta name="robots" content="index,follow"/>
+            <meta name="robots" content="index, follow"/>
             {/* <meta name='keywords' content='content keywords' /> */}
             {/* <meta name='author' content='Flavio Ripa' /> */}
             {/* <meta name='robots' content='index, follow' /> */}
@@ -25,7 +25,7 @@ const RootLayout = ({children}) => {
             <meta property='og:description' content="Discover and share AI prompts with your community. Learn new tips and tricks to improve your productivity. Engage with others and expand your knowledge on various topics. Join us now and start your learning journey." /> 
             <meta name="next-head-count" content="8"/>
             <meta property="og:title" content="My prompts" />
-            {/* <meta property='og:image' name="image" content='https://sustpage123333.s3.ap-southeast-2.amazonaws.com/logo.png' /> */}
+            <meta property='og:image' name="image" content='https://sustpage123333.s3.ap-southeast-2.amazonaws.com/logo.png' />
         </head>
         <body>
             <Provider>
